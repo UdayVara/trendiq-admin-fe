97ee87740eac49bffcc1cdb616118787d2a2184c
Uday vara <varaudayd@gmail.com>
2024-10-02 00:06:17 +0530

    Added Enviroment add,edit & list with server actions updated the ui for settings page

---
+++ src/actions/enviroment.actions.ts
@@ -0,0 +1,59 @@
+"use server";
+
+import axiosInstance from "@/utils/axios";
+
+export const fetchEnviroments = async () => {
+  try {
+    const res = await axiosInstance.get("/enviroment");
+
+    if (res.data?.statusCode == 200) {
+      return {
+        data: res.data?.data,
+        message: "Enviroments Fetched Successfully",
+        success: true,
+      };
+    } else {
+      return {
+        success: false,
+        message: res?.data?.message || "Internal Server Error",
+      };
+    }
+  } catch (error) {
+    console.log("Error Occured :- ", error);
+    return { success: false, message:"Internal Server Error" };
+  }
+};
+
+export const createEnviroment = async (data: {
+  key: string;
+  value: string;
+}) => {
+  try {
+    const res = await axiosInstance.post("/enviroment", data);
+    if (res.data?.statusCode == 201) {
+        return {success:true,message:"Enviroment Created Successfully"}
+    }else{
+        return {success:false,message:res?.data?.message || "Internal Server Error"}
+    }
+  } catch (error) {
+    console.log("Error Occured :- ",error)
+    return {success:false,message:"Internal Server Error"}
+  }
+};
+export const updateEnviroment = async (data: {
+  key: string;
+  value: string;
+  id:string;
+}) => {
+  try {
+    const res = await axiosInstance.patch("/enviroment", data);
+    if (res.data?.statusCode == 201) {
+        return {success:true,message:"Enviroment Updated Successfully"}
+    }else{
+        return {success:false,message:res?.data?.message || "Internal Server Error"}
+    }
+  } catch (error) {
+    console.log("Error Occured :- ",error)
+    return {success:false,message:"Internal Server Error"}
+  }
+};
--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -3,10 +3,69 @@ import { Button } from "@/components/ui/button";
 import { DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import { CopyIcon } from "lucide-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MdLockReset } from "react-icons/md";
-
+import { IoMdAdd } from "react-icons/io";
+import { createEnviroment, fetchEnviroments, updateEnviroment } from "@/actions/enviroment.actions";
+import { toast } from "sonner";
+import { LiaEdit } from "react-icons/lia";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Label } from "@/components/ui/label"
 function page() {
+  const [enviroments, setEnviroments] = useState([]);
+  const [open,setOpen] = useState(false)
+  const [value,setValue] = useState({key:"",value:""})
+  const [selectedEnv,setSelectedEnv] = useState<any>(null)
+  const getEnviroments = async () => {
+    const res = await fetchEnviroments()
+
+    if(res.success){
+      setEnviroments(res.data)
+    }else{
+      toast.error(res.message)
+    }
+  }
+
+  const addEditEnviroment = async() => {
+    try {
+
+      if(selectedEnv){
+        const res = await updateEnviroment({...value,id:selectedEnv?.id})
+
+        if(res.success){
+          toast.success(res.message)
+          setOpen(false)
+          getEnviroments()
+        }else{
+          toast.error(res.message)
+        }
+      }else{
+        const res = await createEnviroment(value)
+
+      if(res.success){
+        toast.success(res.message)
+        setOpen(false)
+        getEnviroments()
+      }else{
+        toast.error(res.message)
+      }
+      }
+      
+    } catch (error:any) {
+      toast.error(error?.message || "Internal Server Error")
+    }
+  }
+  useEffect(()=>{
+    getEnviroments()
+  },[])
   return (
     <>
       <div className="container mx-auto">
@@ -19,29 +78,94 @@ function page() {
               Manage Your Prefrence
             </h5>
           </div>
-          <Button title="Reset" className=" text-red-800/90" variant={"ghost"} size={"icon"}><MdLockReset size={35} /></Button>
+          <div className="flex flex-row gap-3">
+            <Button
+              title="Reset"
+              className=" text-red-800/90"
+              variant={"ghost"}
+              size={"icon"}
+            >
+              <MdLockReset size={35} />
+            </Button>
+            <Button title="Add" onClick={()=>{
+              setOpen(true)
+            }} variant={"default"} size={"icon"}>
+              <IoMdAdd size={20} />
+            </Button>
+          </div>
         </div>
-<DropdownMenuSeparator  className="mt-4 h-0.5"/>
+        <DropdownMenuSeparator className="mt-4 h-0.5" />
         <div className="mt-4">
-            <h5 className="text-lg">Add Enviroment Variables</h5>
-            <div className="mt-4">
-                <h3>Key</h3>
-                <Input className="dark:bg-neutral-800"  />
-            </div>
-            <div className="mt-2">
-                <h3>Value</h3>
-                <Input className="dark:bg-neutral-800"  />
-            </div>
-            <Button className="mt-4">Submit</Button>
-
-            <h4 className="mt-6 text-lg mb-4">Enviroment Variables</h4>
-
-            <div className="p-5 shadow flex flex-row items-center gap-3 justify-normal bg-slate-50 rounded dark:bg-neutral-800">
-                <Input value={"thisisrandomstring"} className="bg-slate-300 dark:bg-neutral-900" disabled/>
-                <CopyIcon  />
-            </div>
+          <h4 className="mt-6 text-lg mb-4">Enviroment Variables</h4>
+          {enviroments.map((enviroment:any,index:number) => {
+            return (
+              <div key={index} className="p-5 shadow flex flex-row items-center gap-3 justify-normal bg-slate-50 rounded dark:bg-neutral-800">
+                <Input
+                  value={enviroment.value}
+                  className="bg-slate-300 dark:bg-neutral-900"
+                  disabled
+                />
+                <div className="flex flex-row items-center gap-3">
+                <CopyIcon />
+                <LiaEdit size={25} onClick={()=>{
+                  setSelectedEnv(enviroment)
+                  setOpen(true)
+                }}/>
+                </div>
+              </div>
+            );
+          })}
+          
         </div>
       </div>
+
+      <Dialog open={open} onOpenChange={(value)=>{
+        setSelectedEnv(null)
+        setOpen(value)
+      }}>
+      
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit Enviroment</DialogTitle>
+          <DialogDescription>
+            Make changes to Enviroment Variable here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Key
+            </Label>
+            <Input
+              id="name"
+              placeholder="Enter Key Here"
+              className="col-span-3"
+              defaultValue={selectedEnv?.key ? selectedEnv?.key : ""}
+              onChange={(e)=>{
+                setValue({...value,key:e.target.value})
+              }}
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Value
+            </Label>
+            <Input
+              id="username"
+              placeholder="Enter Value Here"
+              className="col-span-3"
+              defaultValue={selectedEnv?.value ? selectedEnv?.value : ""}
+              onChange={(e)=>{
+                setValue({...value,value:e.target.value})
+              }}
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button onClick={addEditEnviroment} type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
     </>
   );
 }
